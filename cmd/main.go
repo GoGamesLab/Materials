5a92955cedde8fbdd70061561dc4fa0d65ffdcdd
Gomes, J.M. <gomesjmita.br>
2026-06-11 14:29:51 -0300

    cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	materials "github.com/GoGamesLab/Materials/pkg"
 )
 
 var Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 
 func main() {
 	Logger.Info("🔥 Material system started")
+
+	materials.LoadElementsFromJSON("../../Grind/assets/resources/elements.json")
+	materials.LoadSubstancesFromJSON("../../Grind/assets/resources/substances.json")
+	materials.LoadMaterialsFromJSON("../../Grind/assets/resources/materials.json")
 }
--- pkg/materials.go
+++ pkg/materials.go
@@ -8,139 +8,10 @@ import (
 	"strings"
 )
 
-// const (
-// 	AirID MaterialID = iota
-// 	CoalID
-// 	CrudeOilID
-// 	IronBarID
-// 	IronPlateID
-// 	SteelID
-// 	UraniumOreID
-// 	YellowcakeID
-// 	LEUFuelRodID
-// 	DepletedUraniumRodID
-// )
-
 var (
 	Materials  = make(map[MaterialID]Material)
 	Signatures = make(map[string][]MaterialID)
 )
-
-func init() {
-	loadMaterialsFromJSON("../../Grind/assets/resources/materials.json")
-	// RegisterMaterial(Material{
-	// 	ID:   AirID,
-	// 	Name: "Air",
-	// 	composites: []Composite{
-	// 		{PureNitrogenID, 78},
-	// 		{PureOxygenID, 21},
-	// 		{PureArgonID, 1},
-	// 		{CarbonDioxideID, 0.5},
-	// 	},
-	// })
-	// RegisterMaterial(Material{
-	// 	ID:   CoalID,
-	// 	Name: "Coal",
-	// 	composites: []Composite{
-	// 		{PureCarbonID, 80},
-	// 		{PureSulfurID, 5},
-	// 		{MethaneID, 15},
-	// 	},
-	// 	State:       Solid,
-	// 	Temperature: 21,  // Temp ambiente inicial
-	// 	Hardness:    2.5, // Fácil de minerar
-	// })
-	// RegisterMaterial(Material{
-	// 	ID:   CrudeOilID,
-	// 	Name: "Crude Oil",
-	// 	composites: []Composite{
-	// 		{ParaffinID, 60},   // Fração pesada (Betume/Ceras)
-	// 		{MethaneID, 30},    // Fração leve (Gases dissolvidos)
-	// 		{PureSulfurID, 10}, // Impurezas ácidas (Enxofre)
-	// 	},
-	// 	State:       Liquid,
-	// 	Temperature: 21,  // Temperatura ambiente
-	// 	Hardness:    0.0, // Não resiste à mineração (é bombeado)
-	// })
-	// RegisterMaterial(Material{
-	// 	ID:   IronBarID,
-	// 	Name: "Barra de ferro",
-	// 	composites: []Composite{
-	// 		{PureIronID, 98},  // Majoritariamente Ferro
-	// 		{PureSulfurID, 1}, // Com um pouco de impurezas
-	// 		{PureSiliconID, 0.5},
-	// 		{PureManganeseID, 0.25},
-	// 		{PurePhosphorusID, 0.25},
-	// 	},
-	// 	State:       Solid,
-	// 	Temperature: 21,
-	// 	Hardness:    7.0, // Exige ferramentas avançadas
-	// })
-	// RegisterMaterial(Material{
-	// 	ID:   IronPlateID,
-	// 	Name: "Chapa de ferro",
-	// 	composites: []Composite{
-	// 		{PureIronID, 98},  // Majoritariamente Ferro
-	// 		{PureSulfurID, 1}, // Com um pouco de impurezas
-	// 		{PureSiliconID, 0.5},
-	// 		{PureManganeseID, 0.25},
-	// 		{PurePhosphorusID, 0.25},
-	// 	},
-	// 	State:       Solid,
-	// 	Temperature: 21,
-	// 	Hardness:    7.0, // Exige ferramentas avançadas
-	// })
-	// RegisterMaterial(Material{
-	// 	ID:   SteelID,
-	// 	Name: "Steel",
-	// 	composites: []Composite{
-	// 		{PureIronID, 98},  // O aço é majoritariamente Ferro
-	// 		{PureCarbonID, 2}, // Com um pouco de Carbono para dureza
-	// 	},
-	// 	State:       Solid,
-	// 	Temperature: 21,
-	// 	Hardness:    7.0, // Exige ferramentas avançadas
-	// })
-	// RegisterMaterial(Material{
-	// 	ID: UraniumOreID, Name: "Uranium Ore",
-	// 	composites: []Composite{
-	// 		{PureSiliconID, 95},   // Pedra/Quartzo ao redor
-	// 		{UraniumDioxideID, 5}, // Apenas 5% de Urânio real
-	// 	},
-	// 	State:    Solid,
-	// 	Hardness: 4.5,
-	// })
-	// // Yellowcake (Urânio processado e concentrado obtido na refinaria química)
-	// RegisterMaterial(Material{
-	// 	ID: YellowcakeID, Name: "Yellowcake",
-	// 	composites: []Composite{
-	// 		{UraniumDioxideID, 100},
-	// 	},
-	// 	State:    Solid,
-	// 	Hardness: 1.5,
-	// })
-	// // Célula de Combustível Nuclear Enriquecido (Pronto para o Reator)
-	// RegisterMaterial(Material{
-	// 	ID: LEUFuelRodID, Name: "Enriched Uranium Fuel Rod",
-	// 	composites: []Composite{
-	// 		{UraniumDioxideID, 90}, // Urânio denso enriquecido simulado
-	// 		{PureIronID, 10},       // Revestimento protetor metálico (Zircaloy simplificado para Iron)
-	// 	},
-	// 	State:    Solid,
-	// 	Hardness: 6.0,
-	// })
-	// // Barra de Combustível Exaurida (O Lixo Nuclear)
-	// RegisterMaterial(Material{
-	// 	ID: DepletedUraniumRodID, Name: "Depleted Uranium Fuel Rod",
-	// 	composites: []Composite{
-	// 		{Cesium137ID, 60},      // Subproduto de fissão perigoso
-	// 		{PlutoniumOxideID, 10}, // Transmutação de U-238 em Plutônio
-	// 		{PureIronID, 30},
-	// 	},
-	// 	State:    Solid,
-	// 	Hardness: 6.0,
-	// })
-}
 
 func GenerateSignature(composites []Composite) string {
 	// 1. Criar uma cópia para não mexer no original e ordenar por ID
@@ -205,7 +76,7 @@ func Compositions(composite []Composite) ([]Material, error) {
 	return result, nil
 }
 
-func loadMaterialsFromJSON(materialsPath string) error {
+func LoadMaterialsFromJSON(materialsPath string) error {
 
 	// Carregar Materiais
 	mData, err := os.ReadFile(materialsPath)
